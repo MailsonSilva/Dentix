c38f9f1ecd94235d539bf65c6665e3528f6fe7c3
MailsonfcSilva <mailsonfcsilva@gmail.com>
2025-10-17 16:27:44 -0300

    [dyad] Centralizando o conteúdo da página de seleção de procedimento - wrote 1 file(s)

--- src/pages/SelectProcedure.tsx
+++ src/pages/SelectProcedure.tsx
@@ -84,7 +84,7 @@ const SelectProcedure = () => {
   };
 
   return (
-    <div className="container mx-auto p-4 flex flex-col h-screen">
+    <div className="container mx-auto p-4 flex flex-col h-screen justify-center">
       <div className="text-center my-8">
         <h1 className="text-3xl font-bold">Selecione o Procedimento</h1>
         <p className="text-muted-foreground">Escolha um dos procedimentos abaixo para iniciar a simulação.</p>
